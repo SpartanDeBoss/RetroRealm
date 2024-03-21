4376985a6ba9bf7c3667a2807f2804f4538bd8d4
Aaron Moses <pastures77@gmail.com>
2024-03-21 20:44:24 +0100

    Updated Knex version and added migrations directory for managing database schema changes

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -16,6 +16,8 @@ module.exports = {
       directory: path.join(__dirname, '/seeds/development'),
     },
   },
+  // Existing code...
+
   production: {
     client: 'mysql2',
     connection: {
@@ -25,7 +27,10 @@ module.exports = {
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
     },
-    pool: { min: 0, max: 7 },
+    pool: { min: 0, max: 7 }, 
+    migrations: {
+      directory: path.join(__dirname, '/migrations'),
+    },
     seeds: {
       directory: path.join(__dirname, '/seeds/production'),
     },
